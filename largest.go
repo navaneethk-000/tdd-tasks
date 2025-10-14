3ebbcc099ff71c8eada1998e331887097f40c4d5
Navaneeth <navaneethk969@gmail.com>
2025-10-14 17:48:42 +0530

    GREEN : Implements test cases

--- largest.go
+++ largest.go
@@ -1,9 +1,24 @@
 package main
 
+import "fmt"
+
 func Largest(a, b, c int) int {
-	return 3
+	if a > b {
+		if a > c {
+			return a
+
+		} else {
+			return c
+		}
+	} else {
+		if b > c {
+			return b
+		} else {
+			return c
+		}
+	}
 }
 
 func main() {
-
+	fmt.Println(Largest(4, 8, 7))
 }
